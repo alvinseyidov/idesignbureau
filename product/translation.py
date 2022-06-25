10ad3dc1b28618f9e36ba606c84b60feb0e232e0
Elvin Seyidov <alvinseyidov@gmail.com>
2022-06-25 11:57:42 +0300

    'update

--- product/translation.py
+++ product/translation.py
@@ -1,21 +1,4 @@
 from modeltranslation.translator import translator, TranslationOptions
 from .models import *
 
-class CategoryTranslationOptions(TranslationOptions):
-    fields = ('name',)
-
-translator.register(Category, CategoryTranslationOptions)
-
-
-
-
-class RoomTranslationOptions(TranslationOptions):
-    fields = ('name',)
-
-translator.register(Room, RoomTranslationOptions)
-
-
-class ProductTranslationOptions(TranslationOptions):
-    fields = ('name',)
-
-translator.register(Product, ProductTranslationOptions)
+ 
